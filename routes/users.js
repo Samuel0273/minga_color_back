93dd5961546ed61bd634b68f2f508491d05879f0
Samuel García <120742240+Samuel0273@users.noreply.github.com>
2023-06-30 08:53:02 -0500

    register (#9)

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
-import { Router } from "express"
-import create from "../controllers/users/signin.js";
-
+import { Router } from "express";
+import read from "../controllers/users/read.js";
+import register from "../controllers/users/register.js";
+import signin from "../controllers/users/signin.js";
 import validator from "../middlewares/validator.js";
 import userSignin from "../schemas/auth/signin.js";
 
-
-const auth_router = Router()
-
-//auth_router.get()
-auth_router.post('/signin',validator(userSignin) , create);
-
-export default auth_router
+const auth_router = Router();
+auth_router.post("/register", register); //POST: para crear un autor
+auth_router.post("/signin", validator(userSignin), signin);
+auth_router.get("/", read); //GET: para leer (TODOS o SOLO UNO) autores
+//auth_router.put()    //PUT: para actualizar un autor
+//auth_router.delete() //DELETE: para eliminar un autor
+export default auth_router;
