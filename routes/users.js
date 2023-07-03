fe7e79679ffde530a33cf4f636af7ae8487e50ca
MauroMDU <mauro.umfier@hotmail.com>
2023-07-03 18:50:37 -0300

    schema

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,11 @@ import register from "../controllers/users/register.js";
 import signin from "../controllers/users/signin.js";
 import validator from "../middlewares/validator.js";
 import userRegister from "../schemas/auth/register.js";
+import signinSchema from "../schemas/auth/signinSchema.js";
 
 const auth_router = Router();
 auth_router.post("/register", validator(userRegister), register); //POST: para crear un autor
-auth_router.post("/signin", signin);
+auth_router.post("/signin",validator(signinSchema), signin);
 auth_router.get("/", read); //GET: para leer (TODOS o SOLO UNO) autores
 //auth_router.put()    //PUT: para actualizar un autor
 //auth_router.delete() //DELETE: para eliminar un autor
