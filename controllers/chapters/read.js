889d0999c88e6fe735beb1f102dc931bc5638ed0
slash-w <nicotra.santiago98@gmail.com>
2023-07-13 18:22:28 -0300

    task 07 backend

--- controllers/chapters/read.js
+++ controllers/chapters/read.js
@@ -1,32 +1,41 @@
-import Chapter from "../../models/Chapter.js";
+import Chapter from '../../models/Chapter.js'
 
 export default async (req, res) => {
+  const mangaId = req.query.manga_id
+  const page = parseInt(req.query.page) || 1 // pagina actual - 1 default
+  const max_cap = 6 // nro de paginado
+  
   try {
-    let all = await Chapter.find()
-    if (all.length > 0) {
-      return res.status(200).json({
-        response: all,
-        success: true,
-        message: "you have requested GET /api/chapters",
-        mindhub: "the best",
-        date: new Date()
-      })
-    } else {
-      return res.status(404).json({
-        response: null,
-        success: false,
-        message: "Chapters not found",
-        mindhub: "the best",
-        date: new Date()
-      })
+    const totalChapters = await Chapter.countDocuments({ manga_id: mangaId })
+    const totalPages = Math.ceil(totalChapters / max_cap)
+    const chapters = await Chapter.find({manga_id: mangaId})
+      .select('manga_id title cover_photo pages order')
+      .sort({order: 1}) // Ordenar los capítulos por "order" de forma ascendente
+      .collation({locale: "en", numericOrdering: true}) // Collation, numericOrdering: compara numeros str como int - locale: el idioma
+      .skip((page - 1) * max_cap) // Saltar los capítulos anteriores a la página actual
+      .limit(max_cap) // Limitar el número de capítulos a mostrar por página
+
+    const response = {
+        chapters: chapters,
+        currentPage: page,
+        totalPages: totalPages,
     }
-  }
-  catch (error){
-    console.log(error)
-    return res.status(500).json({
-      response: null,
-      success: false,
-      message: error.message,
-    })
+
+    if (page > 1) {
+        response.prev = true
+        response.next = false
+      }
+    else if (page < totalPages) {
+        response.prev = false
+        response.next = true
+    }
+
+    res.json(response)
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error al obtener los capítulos' })
   }
 }
+
+//C:\Users\jsnla\OneDrive\Escritorio\PyProjects\mindhub\MERN\modulo-MERN\sprint2\serompiotodo\minga_color_back\controllers\chapters\read.js
