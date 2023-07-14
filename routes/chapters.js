8db2c3414c18a2ab5e1392642ab3d8e7537036d3
Samuel García <samuelgarciam03@gmail.com>
2023-07-14 00:56:29 -0500

    endpoint 08

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import read from "../controllers/chapters/read.js";
+import read_one from "../controllers/chapters/read_one.js";
+import passport from "../middlewares/passport.js";
 
 const chapters_router = Router();
 
 //chapters_router.post()   //POST: para crear un autor
-chapters_router.get("/", read); //GET: para leer (TODOS o SOLO UNO) autores
+chapters_router.get(
+  "/:id/",
+  passport.authenticate("jwt", { session: false }),
+  read_one
+); //GET: para leer (TODOS o SOLO UNO) autores
 //chapters_router.put()    //PUT: para actualizar un autor
 //chapters_router.delete() //DELETE: para eliminar un autor
 
